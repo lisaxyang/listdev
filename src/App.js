da9c02a0c2ea4c2fc6d087f8ce9a64e833dccd0e
lisaxyang <66920418+lisaxyang@users.noreply.github.com>
2020-12-02 10:40:19 -0500

    got layout and am trying to filter 2 now

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FilteredList from "./FilteredList";
 
 const usStatesList = [
--- src/FilteredList.js
+++ src/FilteredList.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
-import { Nav } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import DisplayList from "./DisplayList";
 
 class FilteredList extends Component {
@@ -8,22 +11,43 @@ class FilteredList extends Component {
   constructor() {
     super();
     this.state = { 
-      size: "All",
+      color: "All",
+      region: "All",
     };
   }
 
-  onSelectFilterSize = event => {
+  onSelectFilterColor = event => {
     this.setState({
-      size: event
+      color: event
     })
   };
   
 
-  matchesFilterSize = item => {
+  matchesFilterColor = item => {
     // all items should be shown when no filter is selected
-    if(this.state.size === "All") { 
+    if(this.state.color === "All") { 
+      return this.matchesFilterRegion;
       return true;
-    } else if (this.state.size === item.color) {
+    } else if (this.state.color === item.color) {
+      return this.matchesFilterRegion;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onSelectFilterRegion = event => {
+    this.setState({
+      region: event
+    })
+  };
+  
+
+  matchesFilterRegion = item => {
+    // all items should be shown when no filter is selected
+    if(this.state.region === "All") { 
+      return true;
+    } else if (this.state.region === item.region) {
       return true;
     } else {
       return false;
@@ -38,14 +62,79 @@ class FilteredList extends Component {
       // </div>
       // Contains filter and sort options
       <div>
-        <Nav.Item>
-          <Nav.Link eventKey="All" onSelect={this.onSelectFilterSize}>All</Nav.Link>
-          <Nav.Link eventKey="Blue" onSelect={this.onSelectFilterSize}>Blue</Nav.Link>
-          <Nav.Link eventKey="Red" onSelect={this.onSelectFilterSize}>Red</Nav.Link>
-          <Nav.Link eventKey="Swing" onSelect={this.onSelectFilterSize}>Swing</Nav.Link>
-        </Nav.Item>
+        <link rel="stylesheet" href="onlinebootstrap.css"></link>
+        <Container fluid style={{backgroundColor: "grey"}}>
+          <Row>
+            <Col md lg="7" style={{backgroundColor: "green"}}>
+              <Row style={{backgroundColor: "pink"}}>
+                <Nav.Item>
+                  <Nav.Link eventKey="All" onSelect={this.onSelectFilterColor}>All</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link eventKey="Blue" onSelect={this.onSelectFilterColor}>Blue</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="Red" onSelect={this.onSelectFilterColor}>Red</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="Swing" onSelect={this.onSelectFilterColor}>Swing</Nav.Link>
+                </Nav.Item>
+              </Row>
+              <Row style={{backgroundColor: "pink"}}>
+                <Nav.Item>
+                  <Nav.Link eventKey="All" onSelect={this.onSelectFilterColor}>All</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link eventKey="Midwest" onSelect={this.onSelectFilterColor}>Midwest</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="Northeast" onSelect={this.onSelectFilterColor}>Northeast</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="South" onSelect={this.onSelectFilterColor}>South</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="West" onSelect={this.onSelectFilterColor}>West</Nav.Link>
+                </Nav.Item>
+              </Row>
+      
+              <Row style={{backgroundColor: "honeydew"}}>
+                <Nav.Item>
+                  <Nav.Link eventKey="All" onSelect={this.onSelectFilterRegion}>All</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link eventKey="Midwest" onSelect={this.onSelectFilterRegion}>Midwest</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="Northeast" onSelect={this.onSelectFilterRegion}>Northeast</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="South" onSelect={this.onSelectFilterRegion}>South</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="West" onSelect={this.onSelectFilterRegion}>West</Nav.Link>
+                </Nav.Item>
+              </Row>
 
-        <DisplayList list={this.props.list.filter(this.matchesFilterSize)}/>
+              <Row>
+                <DisplayList list={this.props.list.filter(this.matchesFilterColor)}/>
+              </Row>
+            </Col>
+  
+            <Col style={{backgroundColor: "yellow"}}>
+              <p>Aggregator here</p>
+            </Col>
+  
+            {/* <Col style={{backgroundColor: "purple"}}>
+              <p>hello</p>
+            </Col>
+  
+            <Col style={{backgroundColor: "lavender"}}>
+              <p>sad</p>
+            </Col> */} 
+          </Row>
+
+        </Container>
       </div>
     )
   }
